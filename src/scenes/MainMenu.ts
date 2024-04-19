32e8a41a16f7d3277faf397005f8b6dd4240f779
craig beswetherick <craigbeswetherick@gmail.com>
2024-04-19 06:49:12 +0100

    lint

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,32 +1,32 @@
-import { Scene, GameObjects } from 'phaser';
+import { Scene, GameObjects } from "phaser";
 
-export class MainMenu extends Scene
-{
-    background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
+export class MainMenu extends Scene {
+  background: GameObjects.Image;
+  logo: GameObjects.Image;
+  title: GameObjects.Text;
 
-    constructor ()
-    {
-        super('MainMenu');
-    }
+  constructor() {
+    super("MainMenu");
+  }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+  create() {
+    this.background = this.add.image(512, 384, "background");
 
-        this.logo = this.add.image(512, 300, 'logo');
+    this.logo = this.add.image(512, 300, "logo");
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.title = this.add
+      .text(512, 460, "Main Menu", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Game');
-
-        });
-    }
+    this.input.once("pointerdown", () => {
+      this.scene.start("Game");
+    });
+  }
 }
