2042df5f740d1a51a7c850552b68c40f50a6b7bc
craig beswetherick <craigbeswetherick@gmail.com>
2024-04-23 01:19:28 +0100

    keyboard amends

--- src/Utils/Keyboard.ts
+++ src/Utils/Keyboard.ts
@@ -29,32 +29,24 @@ class Keyboard extends Phaser.GameObjects.Container {
   private initializeKeys(): void {
     const scale = getGameScale(Config);
     const scaledMaxWidth = window.innerWidth;
-    const dynamicPadding = scaledMaxWidth * 0.02; // Adjusted padding
 
     const keyHeight = this.config.fontSize * 1.5 * scale;
     const rows = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
+    const longestRowLength = Math.max(...rows.map((row) => row.length));
+    const dynamicPadding = scaledMaxWidth * 0.02; // Fixed padding percentage of total width
+
+    // Calculate the width available for all keys after accounting for padding
+    const totalPadding = dynamicPadding * (longestRowLength - 1);
+    const availableWidth = scaledMaxWidth - totalPadding;
+    const keyWidth = availableWidth / longestRowLength;
 
     rows.forEach((row, rowIndex) => {
-      const rowWidths = row.split("").map((char) => {
-        const tempText = this.scene.add.text(0, 0, char, {
-          font: `${this.config.fontSize * scale}px ${this.config.font}`,
-          color: "#000000",
-        });
-        const textWidth = tempText.width;
-        tempText.destroy();
-        return textWidth;
-      });
-
-      const totalRowWidth =
-        rowWidths.reduce((acc, width) => acc + width, 0) +
-        dynamicPadding * (row.length - 1);
-      const xOffset = (scaledMaxWidth - totalRowWidth) / 2;
-
-      let xPosition = xOffset;
+      let xPosition =
+        (scaledMaxWidth - (keyWidth * row.length + totalPadding)) / 2;
 
       row.split("").forEach((char, index) => {
         const key = addText(
-          xPosition + rowWidths[index] / 2,
+          xPosition + keyWidth / 2,
           rowIndex * (keyHeight + dynamicPadding),
           this.config.font,
           this.config.fontSize * scale,
@@ -68,8 +60,8 @@ class Keyboard extends Phaser.GameObjects.Container {
         key.on(KeyboardEvent.keyPressed, () => this.handleKeyPress(char));
         this.keys.push(key);
 
-        // Move the position to the next key, accounting for the current key's width and padding
-        xPosition += rowWidths[index] + dynamicPadding;
+        // Update xPosition for the next key
+        xPosition += keyWidth + dynamicPadding;
       });
     });
   }
